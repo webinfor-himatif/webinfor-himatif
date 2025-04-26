8b28fed5da61ce168446344fbd8d3ba9b59c38b0
Dimas Candra <98811223+dimascandraa@users.noreply.github.com>
2025-04-26 14:45:57 +0700

    penambahan devisi BPH

--- src/components/Divisi.js
+++ src/components/Divisi.js
@@ -5,11 +5,14 @@ const Divisi = () => {
         <div className="divisi" id='divisi'>
             <p className='title'>Daftar Divisi .</p>
             <div className="listDivisi">
+                <a href="#bph" className='divisiImg bph'></a>
                 <a href="#humas" className='divisiImg humas'></a>
                 <a href="#litbang" className='divisiImg litbang'></a>
                 <a href="#kwu" className='divisiImg kwu'></a>
                 <a href="#kaderan" className='divisiImg kaderan'></a>
-                {/* <img src={kadivHumas} alt="" className='divisiImg humas'/>
+                {/* 
+                <img src={kadivBph} alt="" className='divisiImg bph'/>
+                <img src={kadivHumas} alt="" className='divisiImg humas'/>
                 <img src={kadivLitbang} alt="" className='divisiImg litbang'/>
                 <img src={kadivKwu} alt="" className='divisiImg kwu'/>
                 <img src={kadivKaderan} alt="" className='divisiImg kaderan'/> */}
